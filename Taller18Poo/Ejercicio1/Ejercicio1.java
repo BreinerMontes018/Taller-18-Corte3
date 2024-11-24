c81a35312022b5e85a9400a564077d21490262c1
BreinerMontes01 <breiner.montespetro@unicolombo.edu.co>
2024-11-23 21:26:13 -0500

    Taller 18

---
+++ Taller18Poo/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,12 @@
+package Taller18Poo.Ejercicio1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        
+        Reparacion taller = new Taller();
+        Limpieza servicioLimpieza = new ServicioDeLimpieza();
+
+        taller.reparar();
+        servicioLimpieza.limpiar();
+    }
+}
